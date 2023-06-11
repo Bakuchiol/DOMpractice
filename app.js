d8915b24add1e8e37478070c98192a4a6b025642
Lynette <lynettecargo@Lynettes-Air.home>
2023-06-11 11:49:54 -0500

    added objects

---
+++ app.js
@@ -0,0 +1,172 @@
+const womens = {
+    mainImage: "https://im.uniqlo.com/global-cms/spa/res1fd53b8da747181302080cdc632b86bdfr.jpg",
+    subNav: "Womens",
+    mainTitle: "WOMENS",
+    mainTitle: "U Crew Neck T-Shirts",
+    mainDesc: "An Elevated wardrobe essential designed with careful attention to details",
+    mainPrice: "$14.90",
+    productImages: [
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res976e703256bccbcc7bb3ad74e8917050fr.jpg",
+            name: 'T-Shirts',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res3eba18caf0639f1ca03ede073495345cfr.jpg",
+            name: 'UT: Graphic Tees',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res429bb2a8fea1715e9fab02cd8e3314c5fr.jpg",
+            name: 'Bra Tops',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res3df290952450f5137bbcc72267d0392ffr.jpg",
+            name: 'Shirts & Blouses',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res0dc40777769aa428b5851384b4dde936fr.jpg",
+            name: 'Shorts',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/resd937213135757e86673f8d0f86c5671ffr.jpg",
+            name: 'Pants',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res65fe0a4f90ddc226a8289dbcb6c57104fr.jpg",
+            name: 'Dresses & Skirts',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/rese84bbcae408075eaab9937ee7ecc55ddfr.jpg",
+            name: 'Outerwear',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/rese971526608322d1446157a8c13b64693fr.jpg",
+            name: 'Sport Utility Wear',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/resdca61ec5e39b407e9b080d672e8d5d22fr.jpg",
+            name: 'Innerwear',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/resaed3b8d226aeb44840715432040a8524fr.jpg",
+            name: 'Accessories',
+        },
+]
+}
+// kids
+const kids = {
+    mainImage: "https://im.uniqlo.com/global-cms/spa/resa1f97a1bbcf0e402ab9df31f14ce98d9fr.jpg",
+    subNav: "Kids",
+    mainTitle: "KIDS",
+    mainTitle: "U AIRism Cotton Crew Neck T-Shirts",
+    mainDesc: "Smooth, quick-drying AIRism fabric with the look of cotton.",
+    mainPrice: "$14.90",
+    productImages: [
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/resc9a99d8b1c93aed1506cf8609832b1f0fr.jpg",
+            name: 'T-Shirts',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res173dbe738e7e719a97e6da47f5f897f8fr.jpg",
+            name: 'UT: Graphic Tees',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/resdce1882f2a01462fdca23b110c2bfce5fr.jpg",
+            name: 'Sweatshirts & Hoodies',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res749c33b0490300f311015c91e9257aaffr.jpg",
+            name: 'Shorts',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res0dc40777769aa428b5851384b4dde936fr.jpg",
+            name: 'Shorts',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res24dec372bda757b883e6bae2291dbe6afr.jpg",
+            name: 'Jeans',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res8c9e7cb2afd48681a6d38d8cbe5cbfd0fr.jpg",
+            name: 'Pants',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res74dcb689dcd74598861c7a83c8330474fr.jpg",
+            name: 'Dresses',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res59416b2db587371b89ae545983e0f510fr.jpg",
+            name: 'Sport Utility Wear',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res88c743e558713367dec36aafdbb35245fr.jpg",
+            name: 'Outerwear',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/rese5558629811c2af1c7380befef080abefr.jpg",
+            name: 'Innerwear',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res40d3467b45545991f7cf615eb64c0d8dfr.jpg",
+            name: 'Accessories',
+        },
+]
+}
+// baby
+const baby = {
+    mainImage: "https://im.uniqlo.com/global-cms/spa/rese0655785eaaf5d81ac8ee4aa98214a6afr.jpg",
+    subNav: "Baby",
+    mainTitle: "BABY",
+    mainTitle: "Baby Matching Sets",
+    mainDesc: "Matching sets for the little ones.",
+    mainPrice: "",
+    productImages: [
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res6731d52399131b9ab63750e07a07923dfr.jpg",
+            name: 'Tops',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/resf4ee67e605b8d986adbfce00d40b4301fr.jpg",
+            name: 'Bodysuits',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res3ae21a6493b12c8bc322da0d22e14d7dfr.jpg",
+            name: 'Pajamas',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res5f4e02f972caa03f35223ad7b51c6810fr.jpg",
+            name: 'Outerwear',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res76ac6c8f6d524961740dedbf12df7500fr.jpg",
+            name: 'Shorts',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res55251d8bed82a4ec53f07ab0b5cf0cc5fr.jpg",
+            name: 'Cropped Leggings',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/res4b64e0abff3fa2cb71f2e4cb37152a2ffr.jpg",
+            name: 'Bottoms',
+        },
+        {
+            pic: "https://im.uniqlo.com/global-cms/spa/resf87388342c780b2242aed358967cc439fr.jpg",
+            name: 'Socks',
+        },
+        {
+            pic: "https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/444926/item/goods_65_444926.jpg?width=300",
+            name: 'Sport Utility Wear',
+        },
+        {
+            pic: "https://image.uniqlo.com/UQ/ST3/us/imagesgoods/458440/item/usgoods_54_458440.jpg?width=300",
+            name: 'Outerwear',
+        },
+        {
+            pic: "https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/454937/item/goods_10_454937.jpg?width=300",
+            name: 'Innerwear',
+        },
+        {
+            pic: "https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/460790/item/goods_32_460790.jpg?width=300",
+            name: 'Accessories',
+        },
+]
+}
